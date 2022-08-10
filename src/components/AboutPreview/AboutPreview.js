10217f0ae23006f8bd42c14fdd6395a3786f9b71
Piotr <zalewskipiotr050@gmail.com>
2022-08-10 12:34:42 +0200

    Add About

---
+++ src/components/AboutPreview/AboutPreview.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import styled from 'styled-components';
+import Image from 'gatsby-image';
+
+const DivContent = styled.div`
+  float: left;
+  width: 50%;
+  height: 100%;
+`;
+
+const StyledImage = styled(Image)`
+  position: absolute !important;
+  height: 100%;
+  top: 0;
+  right: 0;
+  width: 50%;
+  object-fit: cover;
+`;
+
+const AboutContentP = styled.p`
+  font-size: 20px;
+  padding: 30px 65px 0 0;
+`;
+
+const Authorp = styled.p`
+  font-weight: bold;
+  font-size: 40px;
+`;
+
+const StyledHr = styled.hr`
+  position: absolute !important;
+  border: 2px solid black;
+  left: 0;
+  width: 50%;
+  object-fit: cover;
+`;
+
+const AboutPreview = ({ image, author, aboutContent }) => (
+    <>
+        <DivContent>
+            <StyledHr />
+            <AboutContentP>{aboutContent}</AboutContentP>
+            <Authorp>{author}</Authorp>
+            <StyledHr />
+        </DivContent>
+        <StyledImage fluid={image} />
+    </>
+);
+
+export default AboutPreview;
--- src/pages/about.js
+++ src/pages/about.js
@@ -1,12 +1,36 @@
-import * as React from "react"
+import * as React from "react";
+import PageInfo from "../components/PageInfo/Pageinfo";
+import { graphql } from "gatsby";
+import AboutPreview from "../components/AboutPreview/AboutPreview";
 
-const AboutPage = () => {
+const pageData = {
+  title: 'about',
+  paragraph: `While artists work from real to the abstract, architects must work from the abstract to the real.`,
+}
+
+const AboutPage = ({ data }) => {
+  //console.log(data)
   return (
     <>
-      <p>Test</p>
+      <PageInfo title={pageData.title} paragraph={pageData.paragraph} />
+      <AboutPreview image={data.datoCmsAbout.imageAuthor.fluid} author={data.datoCmsAbout.author} aboutContent={data.datoCmsAbout.aboutContent}/>
     </>
   )
 }
+
+export const query = graphql`
+  {
+    datoCmsAbout {
+      aboutContent
+      author
+      imageAuthor {
+        fluid(maxWidth: 800, maxHeight: 1200) {
+          ...GatsbyDatoCmsFluid_tracedSVG
+        }
+      }
+    }
+  }
+`;
 
 export default AboutPage
 
